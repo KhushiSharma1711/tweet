3e505b6ee3546f0d350907230201ed611a29f7fc
KhushiSharma1711 <khushisharma89173@gmail.com>
2025-03-06 10:43:22 +0000

    commit 2

--- todo/todos/urls.py
+++ todo/todos/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
+from .views import IndexView, add, delete, update 
 
 app_name='todos'
 
@@ -7,5 +8,7 @@ urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
     path('<int:todo_id>/delete', views.delete, name='delete'),
     path('<int:todo_id>/update', views.update, name='update'),
+    path('edit/<int:todo_id>/', update, name='edit'),
     path('add/', views.add, name='add')
 ]
+
--- todo/todos/views.py
+++ todo/todos/views.py
@@ -3,6 +3,7 @@ from django.views import generic
 from django.http import HttpResponseBadRequest
 from .models import Todo
 
+
 # List all todos
 class IndexView(generic.ListView):
     template_name = 'index.html'
@@ -11,6 +12,17 @@ class IndexView(generic.ListView):
     def get_queryset(self):
         """Return all todos, ordered by latest created."""
         return Todo.objects.order_by('-created_at')
+
+
+
+def todo_list(request):
+    tasks = Todo.objects.all()
+    completed_tasks_count = Todo.objects.filter(completed=True).count()
+    return render(request, 'index.html', {
+        'tasks': tasks,
+        'completed_tasks_count': completed_tasks_count
+    })        
+
 
 # Add a new todo
 def add(request):
@@ -22,16 +34,44 @@ def add(request):
         return HttpResponseBadRequest("Title cannot be empty")
     return redirect('todos:index')
 
+
 # Delete a todo
 def delete(request, todo_id):
     todo = get_object_or_404(Todo, pk=todo_id)
     todo.delete()
     return redirect('todos:index')
 
+
+
+# # Update a todo
+# def update(request, todo_id):
+#     if request.method == 'POST':
+#         todo = get_object_or_404(Todo, pk=todo_id)
+#         todo.isCompleted = bool(request.POST.get('isCompleted'))
+#         todo.save()
+#     return redirect('todos:index')
+
+
+# # Update a todo
+# def update(request, todo_id):
+#     if request.method == 'POST':
+#         todo = get_object_or_404(Todo, pk=todo_id)
+#         todo.title = request.POST.get('title', todo.title)  # Allow title update
+#         todo.isCompleted = 'isCompleted' in request.POST  # Checkbox handling
+#         todo.save()
+#     return redirect('todos:index')
+
+
+
 # Update a todo
 def update(request, todo_id):
+    todo = get_object_or_404(Todo, pk=todo_id)
     if request.method == 'POST':
-        todo = get_object_or_404(Todo, pk=todo_id)
-        todo.isCompleted = bool(request.POST.get('isCompleted'))
+        todo.title = request.POST['title']
+        todo.isCompleted = 'isCompleted' in request.POST 
         todo.save()
-    return redirect('todos:index')
+        return redirect('todos:index')  # Redirect to the list view
+    
+    return render(request, "edit.html", {'todo': todo})
+
+
